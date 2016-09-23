fb8638e78d73bdba9257bdb2ea6194b266301a9d
EconJoe <staudt.joe@gmail.com>
2016-09-23 07:28:18 -0400

    Add files via upload

--- osm.pl
+++ osm.pl
@@ -1,18 +1,26 @@
 
 #!/usr/bin/perl -w
 
+#http://search.cpan.org/~pauamma/Geo-PostalAddress-0.04/PostalAddress.pm
+
 use strict;
 #use warnings;
 use Geo::Parse::OSM;
 use Data::Dumper;
 
+my $path="C:\\Users\\JoeS\\Downloads";
+
 open (OUTFILE1, ">Dumped.txt") or die "Can't open subjects file: Dumped.txt";
+
 open (OUTFILE2, ">Test.txt") or die "Can't open subjects file: Test.txt";
+print OUTFILE2 "type	id	visible	timestamp	lat	lon	user	changeset	uid	";
+print OUTFILE2 "amenity	source	website	phone	openinghours	cuisine	name	wikipedia	internetaccess	";
+print OUTFILE2 "addrcity	addrpostcode	addrstreet	addrhousenumber\n";
 
 
-Geo::Parse::OSM->parse_file( 'map.osm', sub{ print OUTFILE1 Dumper $_[0] } );
+#Geo::Parse::OSM->parse_file( "$path\\ex_HsvMczpFRQizva2AW4gLHbGmTSP3k.osm.bz2", sub{ print OUTFILE1 Dumper $_[0] } );
 
-Geo::Parse::OSM->parse_file( 'map.osm',
+Geo::Parse::OSM->parse_file( "$path\\ex_HsvMczpFRQizva2AW4gLHbGmTSP3k.osm.bz2",
       sub{
           my $id="$_[0]->{id}";
           my $visible="$_[0]->{visible}";
@@ -27,6 +35,12 @@ Geo::Parse::OSM->parse_file( 'map.osm',
           my $amenity="$_[0]->{tag}->{amenity}";
           my $source="$_[0]->{tag}->{source}";
           my $website="$_[0]->{tag}->{website}";
+          my $phone="$_[0]->{tag}->{phone}";
+          my $openinghours="$_[0]->{tag}->{opening_hours}";
+          my $cuisine="$_[0]->{tag}->{cuisine}";
+          my $name="$_[0]->{tag}->{name}";
+          my $wikipedia="$_[0]->{tag}->{wikipedia}";
+          my $internetaccess="$_[0]->{tag}->{internet_access}";
 
           my $addrcity="addr:city"; $addrcity="$_[0]->{tag}->{$addrcity}";
           my $addrpostcode="addr:postcode"; $addrpostcode="$_[0]->{tag}->{$addrpostcode}";
@@ -34,7 +48,8 @@ Geo::Parse::OSM->parse_file( 'map.osm',
           my $addrhousenumber="addr:housenumber"; $addrhousenumber="$_[0]->{tag}->{$addrhousenumber}";
 
           print OUTFILE2 "$type	$id	$visible	$timestamp	$lat	$lon	$user	$changeset	$uid	";
-          print OUTFILE2 "$amenity	$source	$website	$addrcity	$addrpostcode	$addrstreet	$addrhousenumber\n";
+          print OUTFILE2 "$amenity	$source	$website	$phone	$openinghours	$cuisine	$name	$wikipedia	$internetaccess	";
+          print OUTFILE2 "$addrcity	$addrpostcode	$addrstreet	$addrhousenumber\n";
       } 
 );
 
