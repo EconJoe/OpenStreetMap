5f3f67e85a72b272513f7c87c847004229d50028
EconJoe <staudt.joe@gmail.com>
2016-10-29 05:19:57 -0400

    Add files via upload

--- osm_id.pl
+++ osm_id.pl
@@ -10,7 +10,7 @@ use Data::Dumper;
 
 my $path="B:\\Research\\RAWDATA\\OSM";
 my @states=("alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "district-of-columbia",
-            "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "lousiana", "maine", "maryland",
+            "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland",
             "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire",
             "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahomoa", "oregon", "pennsylvania", "rhode-island",
             "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia",
